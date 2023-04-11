5724446b88c0d81db7897ace9136234f3611aa07
sherekhan9 <Sherekhan9@gmail.com>
2023-04-11 13:14:58 -0500

    MOD08B-KISE

--- Mod08B-Kise/Assets/Scripts/UseData.cs
+++ Mod08B-Kise/Assets/Scripts/UseData.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using static UnityEditor.PlayerSettings;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+using System.Buffers.Text;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
 
 public class UseData : MonoBehaviour
 {/**
@@ -20,6 +25,22 @@ public class UseData : MonoBehaviour
 
     void Awake()
     {
+    //DATA PULLED FROM 
+    /*https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1831
+
+
+        Ground - based Observations of XCO2, XCH4, and XCO, Fairbanks, AK, 2016 - 2019
+
+Description
+        This dataset provides ground - based column - averaged dry mole fractions(DMFs) of CO2(xco2), CO(xco), CH4(xch4), and N2O(xn2o) to supplement satellite - based observations of carbon dynamics of northern boreal ecosystems. Measurements were conducted with Bruker EM27/ SUN Fourier transform spectrometers(FTS) at the University of Alaska Fairbanks(UAF) and two sites on the edges of the Tanana Flats wetlands to the south from 2016 - 08 - 04 to 2019 - 10 - 31.Single detectors were used during the first campaign at UAF in 2017, then two instruments were updated to dual detectors in early 2018 to allow retrieval of xco and xn2o.Data from additional FTS instruments, operated by Los Alamos National Laboratories(LANL), Karlsruhe Institute of Technology(KIT), and Jet Propulsion Laboratory(JPL), employed in these campaigns are included.
+
+
+Data Use and Citation
+Download citation from Datacite
+        RISBibTexOther
+Crosscite Citation Formatter
+Jacobs, N., W.R.Simpson, F.Hase, T.Blumenstock, Q.Tu, M.Frey, M.K.Dubey, and H.A.Parker. 2021.Ground - based Observations of XCO2, XCH4, and XCO, Fairbanks, AK, 2016 - 2019.ORNL DAAC, Oak Ridge, Tennessee, USA. https://doi.org/10.3334/ORNLDAAC/1831
+        This dataset is openly shared, without restriction, in accordance with the EOSDIS Data Use Policy.*/
 
         data = CSVReader.Read("CO2DATA");//udata is the name of the csv file 
 
@@ -28,14 +49,11 @@ public class UseData : MonoBehaviour
             //name, age, speed, description, is the headers of the database
             print("xco2 " + data[i]["xco2"] + " ");
         }
-
-
     }//end Awake()
 
     // Use this for initialization
     void Start()
     {
-
         InvokeRepeating("SpawnObject", startDelay, timeInterval);
     }//end Start()
 
@@ -68,19 +86,12 @@ public class UseData : MonoBehaviour
         tempFloat = System.Convert.ToSingle(tempObj);
         rowCount++;
         temptempFloat = tempFloat;
-      tempFloat = (tempFloat*0.05f);
-
-
-
+        tempFloat = (tempFloat*0.05f);
         Vector3 rot = new Vector3(0 , tempFloat, 0);
-            transform.Rotate(rot);
-
-      
-       transform.localScale = Vector3.Lerp(transform.localScale,new Vector3(tempFloat, tempFloat, tempFloat), Time.deltaTime*300);
+        transform.Rotate(rot);
+        transform.localScale = Vector3.Lerp(transform.localScale,new Vector3(tempFloat, tempFloat, tempFloat), Time.deltaTime*300);
         Debug.Log("The tempFloat is: " + tempFloat);
         Debug.Log("The count is: " + rowCount);
-
-
     }
 
 }
